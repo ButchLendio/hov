c048854045e75460be1b96756c5b98a28d02b2aa
Butch Lendio <butch.lendio@biotechfarms.net>
2021-06-08 13:56:57 +0800

    push

--- #3/index.ts
+++ #3/index.ts
@@ -4,29 +4,35 @@
  * http://www.exceluser.com/formulas/how-to-calculate-both-types-of-compound-growth-rates.html
  * https://www.excelfunctions.net/excel-logest-function.html
  * @param data 
+ * 
+ * 
  */
+ const  SimpleLinearRegression = require("ml-regression-simple-linear")
+
 export default function logest(ys: number[]): number {
   // do something
 
-  const xs = [1,2,3,4,5]
 
-  function solve(x,b,m){
-    const y = b * m**x
-    return y
+  const xs = [0,1,2,3,4]
+  const data =[]
+
+  //filter ys
+  for (let index = 0; index < ys.length; index++) {
+    const element = ys[index];
+    //if index is lower than -1 return NaN value
+    if(element<-1){
+      data.push(NaN)
+    }else{
+      //push the logarithm of the element to data array
+      data.push(Math.log(element))
+    }
   }
+  //execute regression
+  const regression = new SimpleLinearRegression(xs, data);
 
-  const exec = solve(xs,ys,xs)
-  console.log(exec);
-
-  const modelPredictions = solve(xs, exec,ys) 
-  
-  // console.log(modelPredictions)
-
-  const absError = modelPredictions-ys
-
-// console.log(absError);
+  //convert slope to exponential
+  const final = Math.exp(regression.slope)
 
 
-
-  return exec;
+  return final;
 }
